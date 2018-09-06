3849452b65216e5f0256702bc48e1630a2e55578
mcgraths7 <mcgrath.steven7@gmail.com>
2018-09-05 22:11:41 -0400

    created custom collection view cell

---
+++ coder-swag/coder-swag/Controller/ProductsVC.swift
@@ -0,0 +1,20 @@
+//
+//  ProductsVC.swift
+//  coder-swag
+//
+//  Created by Steven McGrath on 9/5/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Steven McGrath on 9/5/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var prodImageView: UIImageView!
+    @IBOutlet weak var prodTitle: UILabel!
+    @IBOutlet weak var prodPrice: UILabel!
+    
+}
