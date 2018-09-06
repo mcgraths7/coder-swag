9ee7c618ba0f2c089c8c79421a1f46c75deb7ace
mcgraths7 <mcgrath.steven7@gmail.com>
2018-09-05 20:12:14 -0400

    created project

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Steven McGrath on 9/5/18.
+//  Copyright © 2018 Steven McGrath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
